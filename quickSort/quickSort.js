5f0f26c9033e80d7ade5025112f4a75aa71ddd6c
Никита Суханов <nik.152002@mail.ru>
2020-10-31 10:58:08 +0300

    Illustrated and tested qsort

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -1,32 +1,27 @@
-// const { assert } = require("chai")
-function swap(arr,firstIndex,secondIndex)
-{
-	[arr[firstIndex],arr[secondIndex]] = [arr[secondIndex],arr[firstIndex]]
-}
+const { assert } = require("chai")
 
 function qsort(arr)
 {
 	const len = arr.length;
-	if(len < 2) 
-		return arr;
-	else{
+	if(len < 2) return arr;
+
 	const pivot = arr[Math.floor(len/2)];
 	const greater = arr.filter(val => val > pivot)
 	const less = arr.filter(val => val < pivot )
-	return [...qsort(less),pivot,...qsort(greater)]
-	}	
+	const equal = arr.filter(val => val == pivot)
+
+	return [...qsort(less),...equal,...qsort(greater)]
 }
 
 
 
 
-
-// describe("quickSort",function () {
-// 	it("should sort array via pivot point with divide & conquer principle",function () {
-// 		assert.deepEqual(quickSort([5,4,3,2,1]),[1,2,3,4,5]);
-// 		assert.deepEqual(quickSort([0]),[0]);
-// 		assert.deepEqual(quickSort([-1,-1,-1]),[-1,-1,-1]);
-// 		assert.deepEqual(quickSort([]),[]);
-// 		assert.deepEqual(quickSort([150,-80,455,198,7]),[-80,7,150,198,455]);
-// 	})
-// })
+describe("qsort",function () {
+	it("should sort array via pivot point with divide & conquer principle",function () {
+		assert.deepEqual(qsort([5,4,3,2,5,1]),[1,2,3,4,5,5]);
+		assert.deepEqual(qsort([0]),[0]);
+		assert.deepEqual(qsort([-1,-1,-1]),[-1,-1,-1]);
+		assert.deepEqual(qsort([]),[]);
+		assert.deepEqual(qsort([150,-80,455,198,7]),[-80,7,150,198,455]);
+	})
+})
